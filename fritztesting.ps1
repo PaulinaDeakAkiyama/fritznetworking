4a3881bb2fec52de595d2b7e277aca13cfd8c4ab
PaulinaDeakAkiyama <paulinadeak@hotmail.com>
2026-04-19 16:22:57 +0100

    creating hashtable to soap format converter and storing password safer

--- fritztesting.ps1
+++ fritztesting.ps1
@@ -1,18 +1,55 @@
-$user = "fritzadmin"
-$pass = "masteroffritz2002"
+# Save
+Get-Credential | Export-Clixml "$env:USERPROFILE\fritz.cred.xml"
+# Load
+$cred = Import-Clixml "$env:USERPROFILE\fritz.cred.xml"
 
-# Get the security port for the Hosts service
+function ConvertTo-SoapArgumentXml {
+  <#
+  .SYNOPSIS
+    Converts a hashtable of arguments into XML elements for a SOAP request.
+  .PARAMETER Arguments
+    A hashtable containing the arguments to convert. Strings musnt contain XML special characters or spaces. Remember to capitalise the first letter of each word.
+  .EXAMPLE
+    $args = @{ NewIndex = 0 }
+    $xml = ConvertTo-SoapArgumentXml -Arguments $args
+    # $xml will contain:
+    # <NewIndex>0</NewIndex>  
+  .OUTPUTS
+    A string containing the XML representation of the arguments.  
+  #>
+    param (
+        [hashtable]$Arguments = @{}
+    )
+
+    if (-not $Arguments -or $Arguments.Count -eq 0) {
+        return ""
+    }
+
+    $lines = foreach ($key in $Arguments.Keys) {
+        $escapedkey = [System.Security.SecurityElement]::Escape([string]$key)
+        $escapedValue = [System.Security.SecurityElement]::Escape([string]$Arguments[$key])
+        "<$escapedkey>$escapedValue</$escapedkey>"
+    }
+
+    return ($lines -join "`n")
+}
 
 function Invoke-FritzRequest {
     param (
         [string]$service,
         [string]$action,
-        [int]$port = 49000
+        [hashtable]$arguments = @{},
+        [int]$port = 49000,
+        [pscredential]$credential
     )
 
     $box = "192.168.178.1"
     $urlPath = $service.ToLower()
-    $url = "http://$box`:$port/upnp/control/$urlPath"
+    if ($port -ne 49000) {
+        $url = "https://$box`:$port/upnp/control/$urlPath"
+    } else {
+        $url = "http://$box`:$port/upnp/control/$urlPath"
+    }
 
     $body = @"
     <?xml version="1.0"?>
@@ -20,6 +57,7 @@ function Invoke-FritzRequest {
     s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <s:Body>
     <u:$action xmlns:u="urn:dslforum-org:service:$service:1">
+    $(ConvertTo-SoapArgumentXml -Arguments $arguments)
     </u:$action>
     </s:Body>
     </s:Envelope>
@@ -30,7 +68,7 @@ function Invoke-FritzRequest {
     "SOAPAction"   = '"urn:dslforum-org:service:{0}:1#{1}"' -f $service, $action
     }
 
-    $response = Invoke-WebRequest -Uri $url -Method POST -Body $body -Headers $headers
+    $response = Invoke-WebRequest -Uri $url -Method POST -Body $body -Headers $headers -Credential $credential
 
     return $response 
 }
@@ -42,7 +80,7 @@ function Get-SecurityPort {
 }
 
 
-$res = Invoke-FritzRequest -service "Hosts" -action "GetGenericHostEntry" -port (Get-SecurityPort)
+$res = Invoke-FritzRequest -service "Hosts" -action "GetGenericHostEntry" -port (Get-SecurityPort) -credential $cred
 
 
 
@@ -68,3 +106,8 @@ for ($i=0; $i -lt $max; $i++) {
     --data "$bodyEntry" `
     "http://$box`:$secureport$hostsControl"
 }
+
+$securePass = ConvertTo-SecureString $pass -AsPlainText -Force
+$credential = [pscredential]::new($user, $securePass)
+
+Invoke-FritzRequest -Service "Hosts" -Action "GetHostNumberOfEntries" -Port (Get-SecurityPort) -Credential (Import-Clixml "$env:USERPROFILE\fritz.cred.xml")
